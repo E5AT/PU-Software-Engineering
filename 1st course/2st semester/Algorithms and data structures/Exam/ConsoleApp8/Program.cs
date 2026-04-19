d592d5f2fea185492ceb388bcb822f9614515831
E5AT <esatcraft@gmail.com>
2026-04-19 22:16:33 +0300

    feat(algorithms): Add initial project for rotting oranges problem

---
+++ 1st course/2st semester/Algorithms and data structures/Exam/ConsoleApp8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+  internal class Program
+  {
+    //0 - blank
+    //1 - fresh
+    //2 - rotten
+    static int Oranges(int[,] oranges)
+    {
+      int minutes, freshOnes;
+      minutes = freshOnes = 0;
+
+      Queue<int> queue = new Queue<int>();
+
+      for (int i = 0; i < oranges.GetLength(0); i++)
+        for (int j = 0; j < oranges.GetLength(1); j++)
+        {
+          if (oranges[i, j] == 1)
+            freshOnes++;
+
+          queue.Enqueue(oranges[i,j]);
+        }
+
+      while (true)
+      {
+
+      }
+    }
+
+    static void Main(string[] args)
+    {
+    }
+  }
+}
