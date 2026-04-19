7843d36535b0d2fad20109e7e69888a0ef22534a
E5AT <esatcraft@gmail.com>
2026-04-19 22:19:40 +0300

    feat(console): implement initial Book class and project structure

---
+++ 1st course/2st semester/OOP2/Exam/ConsoleApp1/ConsoleApp1/Time.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+  internal class Time
+  {
+    private int hour { get; set; }
+    private int minute { get; set; }
+    private int second { get; set; }
+
+    public Time()
+    {
+      hour = minute = second = 0;
+    }
+
+    public Time(int hour, int minute, int second)
+    {
+      this.hour = hour;
+      this.minute = minute;
+      this.second = second;
+    }
+
+    public override string ToString()
+    {
+      return $"{hour.ToString("D2")}:{minute.ToString("D2")}:{second.ToString("D2")}";
+    }
+  }
+}
