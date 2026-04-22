8e4166a8a8e4dfa71944afe40b1d738f9389edec
E5AT <esatcraft@gmail.com>
2026-04-22 17:53:44 +0300

    feat: add ConsoleApp2 project with BankAccount implementation

---
+++ 1st course/2st semester/OOP2/Exam/ConsoleApp2/ConsoleApp2/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+  internal class Product
+  {
+    private string name { get; set; }
+    public double price { get; set; }
+    public int quantity { get; set; }
+
+    public Product(string name, double price, int quantity)
+    {
+      this.name = name;
+      this.price = price;
+      this.quantity = quantity;
+    }
+
+
+    public override string ToString()
+    {
+      return $"Name: {name}, Price: {price}, Quantity: {quantity}";
+    }
+  }
+}
---
+++ 1st course/2st semester/OOP2/Exam/ConsoleApp2/ConsoleApp2/ProductStore.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+  internal class ProductStore
+  {
+    List<Product> products;
+    public ProductStore()
+    {
+      products = new List<Product>();
+    }
+    public void Add(Product product)
+    {
+      products.Add(product);
+    }
+    public override string ToString()
+    {
+      return String.Join("\n", products.Select(x => x.ToString()));
+    }
+    public void ProductsWithPriceUnder20()
+    {
+      foreach(Product product in products)
+        if(product.price<20)
+          Console.WriteLine(product.ToString());
+    }
+    public double TotalSum()
+    {
+      return products.Select(x=> x.price * x.quantity).Sum();
+    }
+  }
+}
