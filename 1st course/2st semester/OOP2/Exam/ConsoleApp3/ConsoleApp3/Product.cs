8a17030d8c199e0f4600e42c70dd7d467a2ea1d3
E5AT <esatcraft@gmail.com>
2026-04-30 14:15:52 +0300

    feat(oop2): initialize ConsoleApp3 project and add Cart class

---
+++ 1st course/2st semester/OOP2/Exam/ConsoleApp3/ConsoleApp3/Product.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+  internal class Product
+  {
+    private string name;
+    public string Name
+    {
+      get { return name; }
+      set
+      {
+        if (value.Length >= 3)
+          name = value;
+        else
+          throw new Exception("Name must be atleast 3 symbols long!");
+      }
+    }
+
+    private double price;
+    public double Price
+    {
+      get
+      {
+        return price;
+      }
+      set
+      {
+        if (value > 0)
+          price = value;
+        else
+          throw new Exception("Price must be greater than 0!");
+      }
+    }
+
+    private DateTime expirationDate;
+    public DateTime ExpirationDate
+    {
+      get { return expirationDate; }
+      set
+      {
+        if (value > DateTime.Today)
+          expirationDate = value;
+        else
+          throw new Exception("Expiration date must be greater than todays date!");
+      }
+    }
+
+    public Product(string name, double price, DateTime expirationDate)
+    {
+      this.name = name;
+      this.price = price;
+      this.expirationDate = expirationDate;
+    }
+  }
+}
---
+++ 1st course/2st semester/OOP2/Exam/ConsoleApp3/ConsoleApp3/Subject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+  internal class Subject
+  {
+    private string name;
+    public string Name
+    {
+      get { return name; }
+      set
+      {
+        if (String.IsNullOrEmpty(value))
+          throw new Exception("Name can't be null or empty!");
+
+        name = value;
+      }
+    }
+
+    public Subject(string name)
+    {
+      Name = name;
+    }
+  }
+
+
+
+
+}
