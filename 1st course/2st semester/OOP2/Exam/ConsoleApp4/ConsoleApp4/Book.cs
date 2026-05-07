179a3b928993ff9a3ce17457e7fb2e335d096b0a
E5AT <esatcraft@gmail.com>
2026-05-07 15:22:23 +0300

    feat: add ConsoleApp4 project and Book class

---
+++ 1st course/2st semester/OOP2/Exam/ConsoleApp4/ConsoleApp4/Book.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+  internal class Book
+  {
+    private string title;
+    public string Title
+    {
+      get { return title; }
+      set
+      {
+        if (string.IsNullOrWhiteSpace(value))
+          throw new ArgumentException();
+
+        title = value;
+      }
+    }
+
+    private string author;
+    public string Author
+    {
+      get { return author; }
+      set
+      {
+        if (string.IsNullOrWhiteSpace(value))
+          throw new ArgumentException();
+
+        author = value;
+      }
+    }
+
+    private string isbn;
+    public string ISBN
+    {
+      get { return isbn; }
+      set
+      {
+        //if (value.Length != 10 || value.Length != 13)
+        //  throw new ArgumentException();
+
+        //if (value.Length == 10)
+        //{
+        //  List<int> digits = value.ToCharArray().Select(x => x - '0').ToList();
+
+        //  int index = 10, sum = 0 ;
+        //  foreach(int digit in digits)
+        //  {
+        //    sum += digit * index;
+        //    index--;
+        //  }
+        //  if(sum%11!=0)
+        //    throw new ArgumentException();
+        //  isbn = value;
+        //}
+        //else if(value.Length == 13)
+        //{
+
+        //}
+
+        isbn = value;
+      }
+    }
+
+
+    private int year;
+    public int Year
+    {
+      get { return year; }
+      set
+      {
+        if (value > DateTime.Today.Year)
+          throw new ArgumentException();
+        year = value;
+      }
+    }
+
+    public override string ToString()
+    {
+      return $"{Title}, {Author}, {ISBN}, {Year}";
+    }
+  }
+}
---
+++ 1st course/2st semester/OOP2/Exam/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp4
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      StreamReader r = new StreamReader("test.txt");
+      string text = r.ReadToEnd();
+      string wordToSearch = Console.ReadLine();
+      Console.WriteLine(text.Contains(wordToSearch)?"Yes, we can find this word in our file":"Nope, this word is not in our file...");
+    }
+  }
+}
